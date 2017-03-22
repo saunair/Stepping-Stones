26bfc64b053a02493df7028bbf82ba3f0c475efd
adityaghadiali <aghadial@andrew.cmu.edu>
2017-03-21 20:58:52 -0400

    Add files via upload

--- Matlab/Scripts/ROSbagscript.m
+++ Matlab/Scripts/ROSbagscript.m
@@ -1,12 +1,16 @@
-directory = [{'rosbags\stance_tests_20th_march'}; {'rosbags\static_com_tests'}];
-size(directory)
-for i=1:size(directory,1)
-    directory{i}
-    file_names = dir((directory{i}));
+directory = [{'stance_tests_20th_march\'}; {'static_com_tests\'}];
+directory1 = [{'..\rosbags\stance_tests_20th_march'}; {'..\rosbags\static_com_tests'}];
+size(directory1);
+for i=1:size(directory1,1)
+    directory1{i}
+    file_names = dir((directory1{i}));
     runsDir = {file_names.name};    
-    
     for j=1:length(runsDir)
-        runsDir{j}
-    end
-    
+        fileparse = runsDir{j}
+        pattern = '.bag';
+        replace = '';
+        fileparse = regexprep(fileparse,pattern,replace)
+        filename = strcat(directory,fileparse);     
+        parseRosbag(filename)
+    end    
 end
