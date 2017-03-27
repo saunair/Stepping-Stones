d12b3e1c1c82867591595e590a512f2bfd710c77
stepping stones <snnair@andrew.cmu.edu>
2017-03-26 22:03:57 -0400

    into test as well

---
+++ catkin_ws/src/morpheus_skates/src/message_integrate.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+#Author: Saurabh Nair
+#Edited BY Aditya Ghadiali and Koushuik 
+import roslib; roslib.load_manifest('morpheus_skates')
+import rospy
+from std_msgs.msg import String
+from morpheus_skates.msg import integrated_message
+import tf
+
+
+publish_rate = rospy.get_param("publish_rate")
+PreloadThreshold = -0.06
+
+total_message = integrated_message()
+
+def left_update(data):
+    global total_message
+    total_message.left = data.left
+
+def right_update(data):
+    global total_message
+    total_message.right = data.right
+
+def pounds_update(data):
+    global total_message, PreloadThreshold
+    total_message.pounds_values = data.pounds_values
+
+def normalize_update(data1):
+	data = data1
+    global total_message, 
+   
+    if data.normalized_force.left_normal_front_outer<0:
+    	if data.normalized_force.left_normal_front_outer<PreloadThreshold:
+    		preload_left_loose = True
+    	data.normalized_force.left_normal_front_outer=0
+    
+
+	if data.normalized_force.left_normal_front_inner<0:
+		if data.normalized_force.left_normal_front_inner<PreloadThreshold:
+    		preload_left_loose = True
+    	data.normalized_force.left_normal_front_inner=0
+    	
+
+	if data.normalized_force.left_normal_rear<0:
+	    if data.normalized_force.left_normal_rear<PreloadThreshold:
+    		preload_left_loose = True	
+    	data.normalized_force.left_normal_rear=0
+    	
+	
+	if data.normalized_force.right_normal_front_outer<0:
+		if data.normalized_force.right_normal_front_outer<PreloadThreshold:
+    		preload_right_loose = True
+		data.normalized_force.right_normal_front_outer=0
+		
+	
+	if data.normalized_force.right_normal_front_inner<0:
+		if data.normalized_force.right_normal_front_inner<PreloadThreshold:
+    		preload_right_loose = True
+    	data.normalized_force.right_normal_front_inner=0
+   
+	
+	if data.normalized_force.right_normal_rear<0:
+        if data.normalized_force.left_normal_rear<PreloadThreshold:
+    		preload_right_loose = True
+    	data.normalized_force.right_normal_rear=0
+
+
+    if preload_right_loose:
+        rospy.logwarn("Fix preload for right")
+    if preload_left_loose:
+        rospy.logwarn("Fix preload for left")
+	
+
+
+    
+    total_message.normalized_force = data.normalized_force
+
+def agg_message_publish():
+    global total_message, publish_rate
+    pub = rospy.Publisher('complete_message', complete_message, queue_size=10)
+    rospy.init_node('feature_integrator', anonymous=True)
+    rate = rospy.Rate(publish_rate) # 100hz
+    listener_trans1 = tf.TransformListener()
+    listener_trans2 = tf.TransformListener()
+    listener_trans3 = tf.TransformListener()
+    listener_trans4 = tf.TransformListener()
+    listener_trans5 = tf.TransformListener()
+
+    rospy.Subscriber("left", String, left_update)
+    rospy.Subscriber("right", String, right_update)
+    rospy.Subscriber("pounds_per_sensor", String, pounds_update)
+    rospy.Subscriber("normalized_force_per_sensor", user_force_normalized, normalize_update)
+
+    while not rospy.is_shutdown():
+        (trans_left_hip,rot1)   = listener_trans1.lookupTransform('/openni_depth_frame', '/left_hip_1', rospy.Time(0))
+        (trans_right_hip,rot1)  = listener_trans2.lookupTransform('/openni_depth_frame', '/right_hip_1', rospy.Time(0))
+        (trans_left_foot,rot1)  = listener_trans3.lookupTransform('/openni_depth_frame', '/left_foot_1', rospy.Time(0))
+        (trans_right_foot,rot1) = listener_trans4.lookupTransform('/openni_depth_frame', '/right_foot_1', rospy.Time(0))
+        (trans_COM,rot1)        = listener_trans5.lookupTransform('/openni_depth_frame', '/centre_mass', rospy.Time(0))
+
+        total_message.centre_of_mass_kinect[0] = trans_COM[0]
+        total_message.centre_of_mass_kinect[1] = trans_COM[1]
+        total_message.centre_of_mass_kinect[2] = trans_COM[2]
+
+        total_message.hip_left[0] = trans_left_hip[0] 
+        total_message.hip_left[1] = trans_left_hip[1]
+        total_message.hip_left[2] = trans_left_hip[2]
+        
+        total_message.hip_right[0] = trans_right_hip[0]
+        total_message.hip_right[1] = trans_right_hip[1]
+        total_message.hip_right[2] = trans_right_hip[2] 
+        pub.publish(total_message)
+        
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        agg_message_publish()
+    except rospy.ROSInterruptException:
+        pass
+
