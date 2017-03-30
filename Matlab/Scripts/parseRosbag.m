ee010d038b67b006a9e61b75206ae38fa444b1c4
bfactor <bfactor@andrew.cmu.edu>
2017-03-30 04:43:28 -0400

    Updates to parseRosbag for command and feedback message changes. Added integrated message but currently results in java memory error. Parsed multiple messages from last rosBag from plotting - noted error in force clipped within integrated message logic.

--- Matlab/Scripts/parseRosbag.m
+++ Matlab/Scripts/parseRosbag.m
@@ -3,12 +3,20 @@ function parseRosbag(filename)
     bag = rosbag(strcat('..\rosbags\',filename,'.bag'));
           
     % Parse available topics  
-    if(isempty(strmatch('/left',bag.AvailableTopics.Properties.RowNames)) == 0)
-        left = parseTopic(bag,'/left','morpheus_skates/skate_feedback');        
+    if(isempty(strmatch('/left_command',bag.AvailableTopics.Properties.RowNames)) == 0)
+        left_cmd = parseTopic(bag,'/left_command','morpheus_skates/skate_command');        
     end
-    
-    if(isempty(strmatch('/right',bag.AvailableTopics.Properties.RowNames)) == 0)
-        right = parseTopic(bag,'/right','morpheus_skates/skate_feedback');
+
+    if(isempty(strmatch('/right_command',bag.AvailableTopics.Properties.RowNames)) == 0)
+        right_cmd = parseTopic(bag,'/right_command','morpheus_skates/skate_command');        
+    end
+ 
+    if(isempty(strmatch('/left_feedback',bag.AvailableTopics.Properties.RowNames)) == 0)
+        left_fbk = parseTopic(bag,'/left_feedback','morpheus_skates/skate_feedback');        
+    end
+
+    if(isempty(strmatch('/right_feedback',bag.AvailableTopics.Properties.RowNames)) == 0)
+        right_fbk = parseTopic(bag,'/right_feedback','morpheus_skates/skate_feedback');        
     end
     
     if(isempty(strmatch('/pounds_per_sensor',bag.AvailableTopics.Properties.RowNames)) == 0)
@@ -19,10 +27,9 @@ function parseRosbag(filename)
         normalized_force_per_sensor = parseTopic(bag,'/normalized_force_per_sensor','morpheus_skates/user_force_normalized');
     end
 
-    if(isempty(strmatch('/servo',bag.AvailableTopics.Properties.RowNames)) == 0)
-        servo = parseTopic(bag,'/servo','morpheus_skates/skate_command');
-    end
-
+%     if(isempty(strmatch('/total_message',bag.AvailableTopics.Properties.RowNames)) == 0)
+%         total_message = parseTopic(bag,'/total_message','morpheus_skates/integrated_message');
+%     end
     %if(isempty(strmatch('/user_position_offset',bag.AvailableTopics.Properties.RowNames)) == 0)
     %    user_position_offset = parseTopic(bag,'/user_position_offset','std_msgs/Float64');
     %end
