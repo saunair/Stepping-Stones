4921a1f3c3788230f86c501c6c93061e5bc3539b
saunair <snnair@andrew.cmu.edu>
2017-04-13 22:44:00 -0400

    COM checks and additional code

--- catkin_ws/src/morpheus_skates/src/state_machine.py
+++ catkin_ws/src/morpheus_skates/src/state_machine.py
@@ -17,6 +17,10 @@ Double_Stance = 1
 Single_Stance_Left = 2 
 Single_Stance_Right = 3
 
+front_stepping = 1
+back_stepping = -1
+stance = 0
+
 current_state = 1
 state_queue = [1,1,1]
 
@@ -28,15 +32,15 @@ SSMR = 3
 DSP  = 4 
 DSM  = 5
 markov_matrix =[[0.40, 0.05, 0.25, 0.05, 0.10, 0.15], 
-               [0.05, 0.40, 0.05, 0.25, 0.10, 0.15],
-               [0.05, 0.10, 0.40, 0.05, 0.15, 0.25],
-               [0.10, 0.05, 0.05, 0.40, 0.15, 0.25],
-               [0.20, 0.20, 0.125,0.125,0.30, 0.05],
-               [0.20, 0.20, 0.125,0.125,0.05, 0.30]]
+                [0.05, 0.40, 0.05, 0.25, 0.10, 0.15],
+                [0.05, 0.10, 0.40, 0.05, 0.15, 0.25],
+                [0.10, 0.05, 0.05, 0.40, 0.15, 0.25],
+                [0.20, 0.20, 0.125,0.125,0.30, 0.05],
+                [0.20, 0.20, 0.125,0.125,0.05, 0.30]]
 
 
 
-## copied one from double stance to initialize`
+## copied one from double stance to initialize
 force_values = [0.0568256229, 0.1217591166, 0.3135216534, 0.0094473129, 0.1839496493, 0.3274969757, 0.5208939314, 0.4921063781, 1.0130003691]
 
 imu_data_left  = 0 
@@ -44,8 +48,8 @@ imu_data_right = 0
 foot_positions = 0
 
 ## used for COM calculations
-dist_rear  = -0.2*10 
-dist_front = 0.8*10
+dist_rear  = -0.25*10 
+dist_front = 0.75*10
 
 #ask for user foot size
 def ask_foot_size():
@@ -63,8 +67,8 @@ def ask_foot_size():
 ### Distance calculations for polygon of support
 def calculate_foot_points(foot_length):
     global dist_rear, dist_front
-    dist_rear  = -0.2*foot_length 
-    dist_front = 0.8*foot_length
+    dist_rear  = -0.25*foot_length 
+    dist_front = 0.75*foot_length
 
 def update_vector(msg):
     global force_values, imu_data_left, imu_data_right, foot_positions
@@ -100,7 +104,7 @@ def update_vector(msg):
                       msg.right_feedback.imu_rate_y,
                       msg.right_feedback.imu_rate_z]
 
-    foot_positions = [msg.foot_left, msg.foot_right]
+    foot_positions = [[msg.foot_left[0], msg.foot_left[1],msg.foot_left[2]], [msg.foot_right[0], msg.foot_right[1], msg.foot_right[2]]]
  
 def markov_decision():
     score_list = no_states*[0] 
@@ -112,26 +116,51 @@ def markov_decision():
 
 def check_polygon(state):   
     global force_values, imu_data_left, imu_data_right, foot_positions
+    global dist_rear, dist_front
     try:
         if state==Single_Stance_Left:
             force_front = (force_values[0] + force_values[1])
             force_rear = force_values[2]
-            COM = float(force_front*dist_front - force_rear*dist_rear)/(force_front + force_rear)
+            COM = float(force_front*dist_front + force_rear*dist_rear)/(force_front + force_rear)
     
         elif state==Single_Stance_Right:
             force_front = (force_values[2] + force_values[3])
             force_rear = force_values[4]
-            COM = float(force_front*dist_front - force_rear*dist_rear)/(force_front + force_rear)
+            COM = float(force_front*dist_front + force_rear*dist_rear)/(force_front + force_rear)
 
         elif state==Double_Stance:
-            force_front = (force_values[0] + force_values[1])
-            force_rear = force_values[2]
-            COM = float(force_front*dist_front - force_rear*dist_rear)/(force_front + force_rear)
+            if(foot_positions[0][0] > foot_positions[1][0]):
+                force_lead_front =  force_values[0] + force_values[1]
+                force_lead_back  =  force_values[2]
+                force_lag_front  =  force_values[3] + force_values[4]
+                force_lag_rear   =  force_values[5]
+            else:
+                force_lag_front  =  force_values[0] + force_values[1]
+                force_lag_back   =  force_values[2]
+                force_lead_front =  force_values[3] + force_values[4]
+                force_lead_rear  =  force_values[5]
+             
+            dist = np.linalg.norm(foot_positions[0], foot_positions[1])
+            
+            COM = float((force_lead_front*(dist_front) +
+                   force_lead_back*(dist_rear) +
+                   force_lag_front*(dist_front - dist) +
+                   force_lag_back*(dist_rear - dist))/(force_lead_front + front_lead_rear + 
+                   force_lag_front + force_lag_rear))
+
+        if COM > (dist_front + eps):    #tolerance
+            motion = front_stepping
+        elif COM < (dist_rear - eps):   #tolerance
+            motion = back_stepping
+        else:
+            motion = 0
+
     except:
         rospy.logwarn('division by zero')
+    return motion
 
 def state_machine(stance_classifier):
-    global force_values, imu_data_left, imu_data_right, foot_positions
+    global force_values, imu_data_left, imu_data_right, foot_positions, front_stepping. back_stepping, stance
     while not rospy.is_shutdown():
         feature_temp = np.asarray(np.asarray(force_values))
 	feature_temp = feature_temp.reshape(1,-1)
@@ -139,21 +168,21 @@ def state_machine(stance_classifier):
         ##append the state and also delete the oldest state
 
         if static_state[0] == Double_Stance:
-            if(check_polygon(static_state)):
+            if(check_polygon(static_state) == front_stepping):
                 state = DSM 
-            else: 
+            elif(check_polygon(static_state) == stance): 
                 state = DSP
 
         elif static_state[0] == Single_Stance_Left:
-            if(check_polygon(static_state)):
+            if(check_polygon(static_state) == front_stepping):
                 state = SSML
-            else:
+            elif(check_polygon(static_state) == stance):
                 state = SSPL
 
         elif static_state[0] == Single_Stance_Right:
-            if(check_polygon(static_state)):
+            if(check_polygon(static_state) == front_stepping):
                 state =  SSMR
-            else:
+            elif(check_polygon(static_state) == stance):
                 state = SSPR
         
         current_state = markov_decision()
@@ -162,7 +191,6 @@ def state_machine(stance_classifier):
         ## append the latest prediction
         state_queue = state_queue.append(state) 
         
-
 
 if __name__=='__main__':
     #rospack = rospkg.RosPack()
