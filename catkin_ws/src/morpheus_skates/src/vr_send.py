b2efb86de63188f818174db2e4ce446ba6e41204
stepping stones <snnair@andrew.cmu.edu>
2017-04-15 02:02:37 -0400

    VR sender updates to node and launch. CL tuning with wheels for Koushik and Brad.

--- catkin_ws/src/morpheus_skates/src/vr_send.py
+++ catkin_ws/src/morpheus_skates/src/vr_send.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-from std_msgs.msg import Floatt32
+from std_msgs.msg import Float32
 
 import rospy
 import time
@@ -17,17 +17,25 @@ from morpheus_skates.srv import *
 publish_rate = rospy.get_param("vr_refresh_rate")
 left_skate_velocity, right_skate_velocity = 0,0
 
-def left_update(left):
-    global left_skate_velocity
-    left_skate_velocity = left.command_target
+# def left_update(left):
+#     global left_skate_velocity
+#     left_skate_velocity = left.left_command.command_target
 
-def rightt_update(right):
-    global right_skate_velocity
-    right_skate_velocity = right.command_target
+# def right_update(right):
+#     global right_skate_velocity
+#     right_skate_velocity = right.right_command.command_target
+
+def update(skate):
+    global left_skate_velocity,right_skate_velocity
+    left_skate_velocity = skate.left_command.command_target
+    right_skate_velocity = skate.right_command.command_target
+
+
 
 def talker():
-    rospy.Subscriber("right_command", skate_command, right_input)
-    rospy.Subscriber("left_command", skate_command, left_update)
+    global left_skate_velocity,right_skate_velocity
+    rospy.Subscriber("total_message", integrated_message, update)
+    # rospy.Subscriber("total_message", skate_command, left_update)
     
     
     rospy.init_node('vr_talker', anonymous=True)
